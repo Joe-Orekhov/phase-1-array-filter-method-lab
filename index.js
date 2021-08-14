c92cc7f1ef121d87d5054dae989a537f3b6a44b4
joesephorekhov <josephorekhov@gmail.com>
2021-08-13 19:58:43 -0600

    commit tree 1e7ca73db11e37f445be11ab3bce01e8cff2ef9e

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution here
+function findMatching(driverArr, name){
+   return driverArr.filter(x => x.toUpperCase().includes(name.toUpperCase()))
+   
+}
+
+function fuzzyMatch(driverArr, name){
+    return driverArr.filter(x => x.startsWith(name))
+}
+
+function matchName(driverArr, name){
+    return driverArr.filter(x => x.name.toUpperCase().includes(name.toUpperCase()))
+}
